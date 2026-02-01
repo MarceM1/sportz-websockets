ea62abd868f0079b6b9377e8a734c2dcf79daf7e
Marcelo Melogno <marcelomelogno1986@gmail.com>
2026-02-01 12:21:59 -0300

    Implement GET and POST routes

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import 'dotenv/config.js'
 import {drizzle} from 'drizzle-orm/node-postgres'
 import pg from 'pg'
 
-if (!process.env.DATABAS_URL){
+if (!process.env.DATABASE_URL){
     throw new Error('DATABASE_URL is not define');
 }
 
